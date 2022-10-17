6f2222082b09196058d2f43e1458f2c1d16562e4
Macintosh HD <macintosh@MacBook-Pro.local>
2022-10-17 08:05:15 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_firebase_login/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.test.management_portal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_firebase_login/app/app.dart';
+import 'package:flutter_firebase_login/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = AppBlocObserver();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final authenticationRepository = AuthenticationRepository();
+  await authenticationRepository.user.first;
+
+  runApp(App(authenticationRepository: authenticationRepository));
+}
